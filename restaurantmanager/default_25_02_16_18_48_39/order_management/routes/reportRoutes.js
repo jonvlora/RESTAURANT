b8898a415f8bc74a532ee8318d368475db7d9f48
jonvlora <103840161+jonvlora@users.noreply.github.com>
2025-02-16 19:14:48 +0000

    Delete restaurantmanager/default_25_02_16_18_48_39/order_management directory

--- restaurantmanager/default_25_02_16_18_48_39/order_management/routes/reportRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reportController = require('../controllers/reportController');
-
-router.get('/sales/daily', reportController.getDailySales);
-router.get('/sales/weekly', reportController.getWeeklySales);
-router.get('/sales/monthly', reportController.getMonthlySales);
-
-module.exports = router;
